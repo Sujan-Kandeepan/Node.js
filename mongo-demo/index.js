e06329709496b40afa70fed102f641d16cb01467
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-07-31 22:15:17 -0400

    Added MongoDB validation examples with existing code

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,25 +4,69 @@ mongoose.connect('mongodb://localhost:27017/mongo-exercises', { useNewUrlParser:
   .catch(error => console.error('Could not connect to MongoDB', error));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { 
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+    // match: /pattern/
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['web', 'mobile', 'network'],
+    lowercase: true,
+    // uppercase: true,
+    trim: true
+  },
   author: String,
-  tags: [ String ],
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function(v, callback) {
+        setTimeout(() => {
+          const result = v && v.length > 0
+          callback(result);
+        }, 2000);
+      },
+      message: 'A course should have at least one tag.'
+    }
+  },
   date: { type: Date, default: Date.now },
-  isPublished: Boolean
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function() { return this.isPublished; },
+    min: 10,
+    max: 200,
+    get: v => Math.round(v),
+    set: v => Math.round(v)
+  }
 });
 
 const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
   const course = new Course({
-    name: 'Angular Course',
-    author: 'Mosh',
-    tags: ['angular', 'frontend'],
-    isPublished: true
+    name: 'Bootstrap Course',
+    category: 'Web',
+    author: 'Rob',
+    tags: ['bootstrap', 'frontend'],
+    isPublished: true,
+    price: 15.8
   });
 
-  const result = await course.save();
-  console.log(result);
+  try {
+    // await course.validate();
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    for (field in ex.errors) {
+      console.log(ex.errors[field].message);
+    }
+  }
+  
 }
 
 async function getCourses() {
@@ -33,6 +77,7 @@ async function getCourses() {
 
   const courses = await Course
     .find({ author: 'Mosh', isPublished: true })
+    // .find({ _id: '5b60fe6f5f544c3adcdde898' })
     // .find({ price: { $gte: 10, $lte: 20 } })
     // .find({ price: { $in: [10, 15, 20] } })
     // .find()
@@ -93,6 +138,5 @@ async function removeCourse2(id) {
   const course = await Course.findByIdAndRemove(id);
   console.log(course);
 }
-
 
 getCourses();
