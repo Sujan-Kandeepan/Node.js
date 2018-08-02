e3c900d4166d02900dd40b028a8eb97154ba3635
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-08-02 16:10:36 -0400

    Added error handling middleware to vidly app

--- vidly/index.js
+++ vidly/index.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
+require('express-async-errors');
 const debug = require('debug')('app:*');
 const config = require('config');
 const mongoose = require('mongoose');
@@ -13,6 +14,7 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const error = require('./middleware/error');
 
 if (!config.get('jwtPrivateKey')) {
   console.log('FATAL ERROR: jwtPrivateKey is not defined.');
@@ -31,6 +33,7 @@ app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
