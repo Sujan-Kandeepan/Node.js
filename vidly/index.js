a1b720ce06a6bfc9e72c55c97fa67c3758f7fc52
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-08-02 18:55:57 -0400

    Handling uncaught exceptions in vidly app

--- vidly/index.js
+++ vidly/index.js
@@ -18,8 +18,24 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const error = require('./middleware/error');
 
+// process.on('uncaughtException', (ex) => {
+//   winston.error(ex.message, ex);
+//   process.exit(1);
+// });
+
+process.on('unhandledRejection', (ex) => {
+  // winston.error(ex.message, ex);
+  // process.exit(1);
+  throw ex; // exception now caught by winston
+});
+
+winston.handleExceptions(new winston.transports.File({ filename: 'uncaught-exceptions.log' }));
+
 winston.add(winston.transports.File, { filename: 'error.log' });
 winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost:27017/vidly', level: 'error' });
+
+const p = Promise.reject(new Error('Failed miserably.'));
+p.then(() => console.log('Done'));
 
 if (!config.get('jwtPrivateKey')) {
   console.log('FATAL ERROR: jwtPrivateKey is not defined.');
